a4a2a37ce6d5690d70e84f31235db5c96f06b382
iceandre <andrea.bronca7@gmail.com>
2025-07-28 15:37:31 +0200

    modificate sostanzialmente tutte le sezioni

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleMenu() {
   document.querySelector('.nav-links').classList.toggle('show');
 }
+
+// Accordion Menù
+const accordions = document.querySelectorAll('.accordion-btn');
+accordions.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+    const panel = btn.nextElementSibling;
+    panel.style.display = (panel.style.display === "block") ? "none" : "block";
+  });
+});
